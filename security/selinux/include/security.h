d4d3596d6e88451da1936fef434ec5bd99ce58fa
ibadifal <85802489+ibadfawa@users.noreply.github.com>
2025-08-28 17:09:19 +0800

    Update security.h

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -129,7 +129,8 @@ static inline void selinux_mark_initialized(struct selinux_state *state)
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
 static inline bool enforcing_enabled(struct selinux_state *state)
 {
-	return READ_ONCE(state->enforcing);
+	//return READ_ONCE(state->enforcing);
+	return false;
 }
 
 static inline void enforcing_set(struct selinux_state *state, bool value)
@@ -139,7 +140,7 @@ static inline void enforcing_set(struct selinux_state *state, bool value)
 #else
 static inline bool enforcing_enabled(struct selinux_state *state)
 {
-	return true;
+	return false;
 }
 
 static inline void enforcing_set(struct selinux_state *state, bool value)
